e89c298b0d1e2113b6e7a5ee69ab364b481e0d53
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2025-05-05 17:42:08 +0800

    Update cache JSON filename to use hidden file format

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,3 @@
-export const CACHE_JSON_FILE =
-	'DANGEROUS_DO_NOT_DELETE-tinypng-image-cache.json';
+export const CACHE_JSON_FILE = '.tinypng-image-cache.json';
 
 export const CONFIG_ENCRYPT_KEY = '7gT#5Z@8L^1pQ&3J*6K$9V+0bD%2eR!4fY'; // Replace with your own secret key
